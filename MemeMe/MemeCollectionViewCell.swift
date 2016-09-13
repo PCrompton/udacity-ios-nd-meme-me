f7db20c18ba53f3af0b7f2618acac9a79fe2d197
Work <work@Pauls-MacBook-Pro.local>
2016-09-13 11:39:34 -0400

    Added missing file reference

---
+++ MemeMe/MemeCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  MemeCollectionViewCell.swift
+//  MemeMe
+//
+//  Created by Paul Crompton on 7/28/16.
+//  Copyright © 2016 Paul Crompton. All rights reserved.
+//
+
+import UIKit
+
+class MemeCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var memeImageView: UIImageView!
+    @IBOutlet weak var topTextLabel: UILabel!
+    @IBOutlet weak var bottomTextLabel: UILabel!
+
+}
